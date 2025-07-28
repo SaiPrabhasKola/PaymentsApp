1f3ff4571ae3aeecf9562f3f5f512d32c99f515b
SaiPrabhasKola <kolasaiprabhas@gmail.com>
2025-07-28 21:50:27 +0530

    made changes to user.js and added middlewares

---
+++ backend/middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization
+        if (!authHeader || authHeader.startsWith('Bearer')) {
+            return res.status(403).json({})
+        }
+
+        const token = authHeader.spilt('')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.userId = decoded.userId
+        next()
+    } catch (error) {
+        return res.status(403).json({})
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const bcrypt = require('bcrypt')
 const z = require('zod');
 const { UserModel } = require('../db');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { authMiddleware } = require('../middleware');
 const signupBody = z.object({
     username: z.email(),
     firstName: z.string(),
@@ -75,9 +76,55 @@ router.post('/signin', async (req, res) => {
             message: `hey ${User.username} you are signed up!!`
         })
     } catch (error) {
-
+        res.json(error)
     }
 
+})
+
+const editBody = z.object({
+
+    currentPassword: z.string(),
+    newPassword: z.string().min(6).optional(),
+    firstName: z.string().optional(),
+    lastName: z.string().optional()
+})
+
+router.put('/edit', authMiddleware, async (req, res) => {
+    try {
+        const result = editBody.safeParse(req.body)
+
+        if (!result.success) {
+            res.status(400).json({
+                message: "error while updating the info"
+            })
+        }
+        const { currentPassword, newPassword, firstName, lastName } = result.data
+        const User = await UserModel.findById(req.userId)
+        const passCheck = await bcrypt.compare(currentPassword, User.password)
+        if (!passCheck) {
+            return res.json({
+                message: "invalid password"
+            })
+        }
+        const payLoad = {};
+        if (firstName !== undefined) {
+            payLoad.firstName = firstName
+        }
+        if (lastName !== undefined) {
+            payLoad.lastName = lastName
+        }
+        if (newPassword) {
+            const newPasswordHash = await bcrypt.hash(newPassword, 10)
+            payLoad.password = newPasswordHash
+        }
+
+        await UserModel.updateOne({ _id: req.userId }, { $set: payLoad })
+        res.json({
+            message: "We've updated your profile"
+        })
+    } catch (error) {
+        res.status(404).json({ error })
+    }
 })
 
 
