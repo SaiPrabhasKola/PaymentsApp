b6deba1ddf201d02c2f6065349e1dbe80dee27d5
SaiPrabhasKola <kolasaiprabhas@gmail.com>
2025-07-29 13:10:39 +0530

    made changes to the middleware and added the feature of editing the userinfo

--- backend/middleware.js
+++ backend/middleware.js
@@ -2,17 +2,22 @@ const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
     try {
+        console.log("handle reached authmiddleware")
         const authHeader = req.headers.authorization
-        if (!authHeader || authHeader.startsWith('Bearer')) {
-            return res.status(403).json({})
+        console.log(authHeader)
+        if (!authHeader) {
+            return res.status(403).json({ message: "no token provided" })
         }
-
-        const token = authHeader.spilt('')[1]
+        console.log('handle')
+        let token = authHeader.split(' ')[1];
+        console.log(`got the token ${token}`)
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded._id)
         req.userId = decoded.userId
+        console.log(req.userId)
         next()
     } catch (error) {
-        return res.status(403).json({})
+        return res.status(403).json({ error: error })
     }
 }
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,6 +5,8 @@ const z = require('zod');
 const { UserModel } = require('../db');
 const jwt = require('jsonwebtoken');
 const { authMiddleware } = require('../middleware');
+
+
 const signupBody = z.object({
     username: z.email(),
     firstName: z.string(),
@@ -51,11 +53,19 @@ router.post('/signup', async (req, res) => {
 router.post('/signin', async (req, res) => {
     try {
         const { username, password } = req.body
+        if (!username || !password) {
+            console.log("creds unavailable")
+            return res.json({
+                message: "please provide the creds"
+            })
+        }
 
         const User = await UserModel.findOne({
             username
         })
         console.log(User)
+        console.log(User._id)
+
         if (!User) {
             return res.status(411).json({
                 message: "user doesn't exist"
@@ -64,22 +74,31 @@ router.post('/signin', async (req, res) => {
         const checkPassword = User.password
         console.log(checkPassword)
 
+        if (!password) { }
         const passCheck = bcrypt.compare(password, checkPassword)
         if (!passCheck) {
             res.status(411).json({
                 message: "invalid creds"
             })
         }
-        const token = jwt.sign(username, process.env.JWT_SECRET)
+        const payLoad = {
+            userId: User._id
+        }
+        const token = jwt.sign(payLoad, process.env.JWT_SECRET)
+
         console.log(token)
+        console.log('handle reached here')
         res.json({
-            message: `hey ${User.username} you are signed up!!`
+            message: `hey ${User.username} you are signed up!!`,
+            token: token
         })
     } catch (error) {
         res.json(error)
     }
 
 })
+
+
 
 const editBody = z.object({
 
@@ -89,23 +108,32 @@ const editBody = z.object({
     lastName: z.string().optional()
 })
 
+
+
 router.put('/edit', authMiddleware, async (req, res) => {
     try {
         const result = editBody.safeParse(req.body)
-
+        if (result.success) {
+            console.log('success parsing the data')
+        }
         if (!result.success) {
             res.status(400).json({
                 message: "error while updating the info"
             })
         }
         const { currentPassword, newPassword, firstName, lastName } = result.data
+        console.log(result)
         const User = await UserModel.findById(req.userId)
+        console.log(`user available ${User.username}`)
         const passCheck = await bcrypt.compare(currentPassword, User.password)
+
         if (!passCheck) {
             return res.json({
                 message: "invalid password"
             })
         }
+
+        console.log('password is correct')
         const payLoad = {};
         if (firstName !== undefined) {
             payLoad.firstName = firstName
@@ -126,6 +154,38 @@ router.put('/edit', authMiddleware, async (req, res) => {
         res.status(404).json({ error })
     }
 })
+
+
+
+
+
+router.get('/users-data', authMiddleware, async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await UserModel.findOne({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+
+
+})
+
 
 
 module.exports = router
